52d749ee8239cc6dbf3a8024f215c4dc03007b97
Anthony Romaniello <aromaniello@ntia.gov>
2020-07-27 16:07:54 -0600

    Added build cleanup

--- Cython API/setup.py
+++ Cython API/setup.py
@@ -14,15 +14,13 @@ import numpy as np
 # 	And then run ldconfig
 api_loc = '/usr/local/lib/RSA_API'
 
-# Clean previous build (not required)
+# Delete previously compiled files
 for root, dirs, files in os.walk(".", topdown=False):
 	for name in files:
 		if (name.startswith('rsa_api') and not(name.endswith(".pyx") or name.endswith(".pxd"))):
 			os.remove(os.path.join(root, name))
-	for name in dirs:
-		if (name == "build"):
-			shutil.rmtree(name)
 
+# Build Cython module
 setup(ext_modules=cythonize(
 	[Extension('rsa_api',
 		['rsa_api.pyx'],
@@ -32,3 +30,9 @@ setup(ext_modules=cythonize(
 		library_dirs=[api_loc])],
 		compiler_directives={'language_level':'3'})
 )
+
+# Delete build directory
+for root, dirs, files in os.walk('.', topdown=False):
+	for name in dirs:
+		if (name == 'build'):
+			shutil.rmtree(name)
