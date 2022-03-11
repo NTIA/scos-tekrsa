b82316c51939855b04666662e9d04a11cff215d9
Anthony Romaniello <66272872+aromanielloNTIA@users.noreply.github.com>
2022-03-11 10:47:03 -0700

    Update radio to sigan for scos-actions update

--- scos_tekrsa/discover/__init__.py
+++ scos_tekrsa/discover/__init__.py
@@ -5,26 +5,26 @@ from scos_actions.actions.monitor_sigan import MonitorSignalAnalyzer
 from scos_actions.actions.sync_gps import SyncGps
 from scos_actions.discover import init
 
-from scos_tekrsa.hardware import radio
+from scos_tekrsa.hardware import sigan
 from scos_tekrsa.settings import ACTION_DEFINITIONS_DIR
 
 # Adjust ACTION_DEFINITIONS_DIR for specific Tektronix analyzer in use
-if radio.device_name in ['RSA306B', 'RSA306']:
+if sigan.device_name in ['RSA306B', 'RSA306']:
     ACTION_DEFINITIONS_DIR += '-300'
-elif radio.device_name in ['RSA503A', 'RSA507A', 'RSA513A', 'RSA518A', 'RSA603A', 'RSA607A']:
+elif sigan.device_name in ['RSA503A', 'RSA507A', 'RSA513A', 'RSA518A', 'RSA603A', 'RSA607A']:
     ACTION_DEFINITIONS_DIR += '-500-600'
 
 logger = logging.getLogger(__name__)
 
 actions = {
-    "monitor_tekrsa": MonitorSignalAnalyzer(radio)
+    "monitor_tekrsa": MonitorSignalAnalyzer(sigan)
 }
 
 # Pass new radio to existing action classes with new SDR specific yaml files
 logger.debug("scos_tekrsa: ACTION_DEFINITIONS_DIR =  " + ACTION_DEFINITIONS_DIR)
-yaml_actions, yaml_test_actions = init(radio=radio, yaml_dir=ACTION_DEFINITIONS_DIR)
+yaml_actions, yaml_test_actions = init(sigan=sigan, yaml_dir=ACTION_DEFINITIONS_DIR)
 actions.update(yaml_actions)
 
 # Support status endpoint
 def get_last_calibration_time():
-    return radio.last_calibration_time
+    return sigan.last_calibration_time
