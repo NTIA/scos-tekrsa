21b3b3836742239c70cff391c4b81540efbeaadf
Anthony Romaniello <aromaniello@ntia.gov>
2022-07-28 14:53:23 -0600

    Fix typo

--- scos_tekrsa/discover/__init__.py
+++ scos_tekrsa/discover/__init__.py
@@ -16,7 +16,7 @@ actions = {"enable_antenna": EnableAntenna(sigan=sigan), 'enable_noise_diode_on'
 logger.info('scos-tekrsa: discovering actions')
 # Adjust ACTION_DEFINITIONS_DIR for specific Tektronix analyzer in use
 if sigan:
-    logger.debug("Devine Name: "  + sigan.device_name)
+    logger.debug("Device Name: "  + sigan.device_name)
     if sigan.device_name in ['RSA306B', 'RSA306']:
         ACTION_DEFINITIONS_DIR += '-300'
     elif sigan.device_name in ['RSA503A', 'RSA507A', 'RSA513A', 'RSA518A', 'RSA603A', 'RSA607A']:
