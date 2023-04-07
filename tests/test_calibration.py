0a7efbf98912d8fa455369e8c8c8e1f51d2cf78b
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-07 18:18:54 -0400

    update example cals and cal unit test

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -17,8 +17,8 @@ class TestCalibration:
         )
         assert sensor_calibration is not None
         assert (
-            sensor_calibration.calibration_data[14000000.0][3555000000][-25][1][0][
-                "noise_figure_sensor"
-            ]
+            sensor_calibration.calibration_data["14000000.0"]["3555000000"]["-25"][
+                "true"
+            ]["0"]["noise_figure_sensor"]
             == 5.0
         )
