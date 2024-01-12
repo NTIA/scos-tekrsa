b686c7ce79a2c5570105c26bc57c49a786b8471f
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-12 06:54:36 -0700

    Only load actions in discover. Use a setting for the device model.

--- src/scos_tekrsa/settings.py
+++ src/scos_tekrsa/settings.py
@@ -13,6 +13,7 @@ CONFIG_DIR = Path(__file__).parent.resolve() / "configs"
 __cmd = Path(sys.argv[0]).name
 
 RUNNING_TESTS = "test" in __cmd
+DEVICE_MODEL = env("DEVICE_MODEL", default="RSA507A")
 
 if not settings.configured or not hasattr(settings, "MOCK_SIGAN"):
     MOCK_SIGAN = env.bool("MOCK_SIGAN", default=False) or RUNNING_TESTS
