7eaf91361c162f3b66dedf83a9a763bdc9b22de4
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-12 10:14:04 -0700

    Don't create sigan.

--- src/scos_tekrsa/hardware/__init__.py
+++ src/scos_tekrsa/hardware/__init__.py
@@ -1,16 +1,1 @@
-import logging
 
-from scos_actions.signals import register_component_with_status
-
-from scos_tekrsa.hardware.tekrsa_sigan import TekRSASigan
-
-logger = logging.getLogger(__name__)
-try:
-    logger.debug(
-        "*********************Creating TekRSASigan******************************"
-    )
-    sigan = TekRSASigan()
-    register_component_with_status.send(sigan, component=sigan)
-except Exception as err:
-    logger.error(f"Unable to create TekRSASigan: {err}")
-    sigan = None
