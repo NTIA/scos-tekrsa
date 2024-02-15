084e21d5c6309c395581e0f3c7a25470d2b86f63
Doug Boulware <dboulware@ntia.doc.gov>
2024-02-15 13:05:34 -0700

    Add SystemExit and RuntimeException actions.

--- src/scos_tekrsa/discover/__init__.py
+++ src/scos_tekrsa/discover/__init__.py
@@ -1,6 +1,8 @@
 import logging
 
 from scos_actions.actions.monitor_sigan import MonitorSignalAnalyzer
+from scos_actions.actions.runtime_exception import RuntimeException
+from scos_actions.actions.system_exit_action import SystemExitAction
 from scos_actions.discover import init
 
 from scos_tekrsa.settings import CONFIG_DIR, DEVICE_MODEL
@@ -29,6 +31,8 @@ else:
     ACTION_DEFINITIONS_DIR = CONFIG_DIR / "actions-500-600"
 logger.debug(f"Action configs directory: {ACTION_DEFINITIONS_DIR}")
 actions["monitor_tekrsa"] = MonitorSignalAnalyzer(parameters={"name": "monitor_tekrsa"})
+actions["system_exit"] = SystemExitAction()
+actions["runtime_exception"] = RuntimeException()
 logger.debug("Created Monitor SIGAN action")
 # Pass new radio to existing action classes with new SDR specific yaml files
 logger.debug("Initializing yaml actions")
